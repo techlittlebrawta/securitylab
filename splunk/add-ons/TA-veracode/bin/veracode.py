173d4579f585226ddd40cf328dd9975824a5d0fe
demaranderson <134936199+demaranderson@users.noreply.github.com>
2024-02-28 16:33:28 -0500

    Update veracode.py

--- splunk/add-ons/TA-veracode/bin/veracode.py
+++ splunk/add-ons/TA-veracode/bin/veracode.py
@@ -7,17 +7,22 @@ import os
 import json
 import logging
 import time
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
+
 # Veracode API base URL for listing applications
 API_BASE = "https://api.veracode.com/appsec/v1/applications"
+
 # Headers for API requests
 HEADERS = {"User-Agent": "Python HMAC Example"}
+
 # Maximum number of retries for API requests
 MAX_RETRIES = 5
+
 # Delay between retries in seconds
 DELAY = 5
- 
+
 # Function to make API request
 def make_api_request(url):
     retries = 0
@@ -36,15 +41,21 @@ def make_api_request(url):
                 raise
     logging.error(f"Failed to make API request to {url} after {MAX_RETRIES} retries.")
     return None
+
 # Function to get all applications
 def get_all_applications():
     url = API_BASE
-    response = make_api_request(url)
-    if not response or "_embedded" not in response:
-        logging.error("No Veracode applications found.")
-        return []
-    return response["_embedded"]["applications"]
- 
+    applications = []
+    while url:
+        response = make_api_request(url)
+        if not response or "_embedded" not in response:
+            logging.error("No Veracode applications found.")
+            return []
+        applications.extend(response["_embedded"]["applications"])
+        # Check for next page
+        url = response["_links"].get("next", {}).get("href")
+    return applications
+
 # Function to get findings
 def get_findings(application_guid):
     url = f"https://api.veracode.com/appsec/v2/applications/{application_guid}/findings?size=500"
@@ -58,12 +69,12 @@ def get_findings(application_guid):
         # Check for next page
         url = response["_links"].get("next", {}).get("href")
     return findings
- 
+
 # Function to sanitize filename
 def sanitize_filename(filename):
     filename = filename.replace('_', ' ')  # replace underscores with spaces
     return "".join(c for c in filename if c.isalnum() or c in (' ', '.', '_')).rstrip()
- 
+
 # Function to save findings as JSON
 def save_findings_as_json(application_name, findings):
     try:
@@ -80,7 +91,7 @@ def save_findings_as_json(application_name, findings):
         logging.error(f"IOError occurred while saving findings: {e}")
     except Exception as e:
         logging.error(f"An unexpected error occurred: {e}")
- 
+
 # Main function
 def main():
     applications = get_all_applications()
@@ -91,7 +102,7 @@ def main():
         application_name = application["profile"]["name"]
         findings = get_findings(application_guid)
         save_findings_as_json(application_name, findings)
- 
+
 # Entry point
 if __name__ == "__main__":
     main()
